fda1d4b311bfc4006963f7ef86a8c28dfb5d525b
Carlos Santisteban <santistebanc@users.noreply.github.com>
2019-06-23 17:14:41 +0200

    version 1

--- generar.js
+++ generar.js
@@ -3,20 +3,22 @@ import { default as $ } from "jquery";
 import activities from "./templates/activities.hbs";
 import activity_entry from "./templates/activity_entry.hbs";
 import link_entry from "./templates/link_entry.hbs";
+import result from "./templates/result.hbs";
+import table from "./templates/table.hbs";
+import code from "./templates/code.hbs";
 Handlebars.registerPartial("activity_entry", activity_entry);
 Handlebars.registerPartial("link_entry", link_entry);
+Handlebars.registerPartial("table", table);
 
-const data = {
-  activities: [{ links: [1] }, { links: [1] }, { links: [1] }, { links: [1] }]
-};
 loadPage();
 
 function addActivity() {
   const template = Handlebars.compile(activity_entry);
-  const new_activity = template();
+  const new_activity = template({ links: [""] });
   const newNode = $(new_activity);
-  newNode.insertBefore("#add_activity");
+  $("#activities_list").append(newNode);
   newNode.find(".remove_activity").click(removeActivity);
+  newNode.find(".add_link").click(addLink);
   newNode.find(".remove_link").click(removeLink);
 }
 
@@ -27,7 +29,6 @@ function addLink() {
   const links_list = $(this)
     .parent()
     .find(".links_list");
-  console.log("......links_list", $(this), links_list);
   links_list.append(newNode);
   newNode.find(".remove_link").click(removeLink);
 }
@@ -46,10 +47,61 @@ function removeLink() {
 
 function loadPage() {
   const template = Handlebars.compile(activities);
+  const data = {
+    activities: [{ links: [""] }]
+  };
   const html = template(data);
-  $("#content").html(html);
+  $("#activities").html(html);
   $("#add_activity").click(addActivity);
   $(".remove_activity").click(removeActivity);
   $(".add_link").click(addLink);
   $(".remove_link").click(removeLink);
+
+  $("#done").click(loadResult);
+}
+
+function loadResult() {
+  const data = {
+    activities: []
+  };
+  $(".activity_entry").each(function() {
+    const new_activity = {};
+    new_activity.visible = $(this)
+      .find(".visible_field")
+      .prop("checked");
+    new_activity.name = $(this)
+      .find(".name_field")
+      .val();
+    new_activity.duration = $(this)
+      .find(".duration_field")
+      .val();
+    new_activity.participants = $(this)
+      .find(".participants_field")
+      .val();
+    new_activity.instructions = $(this)
+      .find(".instructions_field")
+      .val();
+    new_activity.answers = $(this)
+      .find(".answers_field")
+      .val();
+    new_activity.links = [];
+    $(this)
+      .find(".link_entry")
+      .each(function() {
+        const link = $(this)
+          .find(".link_field")
+          .val();
+        new_activity.links.push(link);
+      });
+    data.activities.push(new_activity);
+  });
+
+  const code_template = Handlebars.compile(code);
+  const codebox = code_template({ ...data, json: JSON.stringify(data) });
+
+  data.code = codebox;
+
+  const template = Handlebars.compile(result);
+  const html = template(data);
+  $("#result").html(html);
 }
